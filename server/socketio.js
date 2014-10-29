c9895aa96f451211105e789076986550b83d6990
Bryce Jacobs <popasquat89@gmail.com>
2014-10-28 22:58:16 -0500

    sockets - crossed bridge between master module and client for udpating

--- server/socketio.js
+++ server/socketio.js
@@ -11,12 +11,21 @@ var _socket;
 exports.registerTinkerbell = function () {
   _socket = _io.connect('http://localhost:3000/tinkerbells');
 
-  _socket.emit('tinkerbell:system:state:get', {type: 'Tinkerbell', module: {_id: app.config.module}});
-
-  _socket.on('tinkerbell:system:state:status', function (data) {
-    console.log(data);
-    console.log(Date.now());
+  /**
+   * Return the current status of the system
+   *
+   * @param {Object} data Any data prevelant to the query. // Could be zigbeeNetworkId
+   */
+  _socket.on('tinkerbell:system:state:get', function (data) {
     app.system = data;
+
+    // This should grab the live status of the system.
+    _socket.emit('tinkerbell:system:state:status', {type: 'Tinkerbell', system: {_id: app.config.module}});
+  });
+
+  // The system has been updated from the website / app, propogate all changes.
+  _socket.on('tinkerbell:system:state:put', function (data){
+    console.log(data);
   });
 
   _socket.on('error', function (data) {
