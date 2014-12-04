7e09b2a3f386b1be3740c9d5ab632647a0020bdc
Bryce Jacobs <popasquat89@gmail.com>
2014-12-04 02:48:22 -0600

    pi work

--- server/zigbee.js
+++ server/zigbee.js
@@ -6,12 +6,25 @@ exports.impl.sendUpdate = function (){
     wpi.wiringPiSetupGpio();
 
     wpi.pinMode(18, wpi.modes.OUTPUT);
-    wpi.digitalWrite(18, wpi.HIGH);
-
     wpi.pinMode(23, wpi.modes.OUTPUT);
-    wpi.digitalWrite(23, wpi.HIGH);
-
     wpi.pinMode(24, wpi.modes.OUTPUT);
-    wpi.digitalWrite(24, wpi.HIGH);
 
+    var setting = wpi.HIGH;
+    setInterval(function (){
+        console.log('WRITING %s', setting);
+        setting = switchSetting(setting);
+        
+        wpi.digitalWrite(18, setting);
+        wpi.digitalWrite(23, setting);
+        wpi.digitalWrite(24, setting);
+    }, 5000);
+
+
+    function switchSetting(setting){
+        if(setting === wpi.HIGH){
+            return setting = wpi.LOW;
+        } else {
+            return setting = wpi.HIGH;
+        }
+    }
 };
